4c9ca49abc53e41e37105ba8d97152ac61bca5cb
gollilla <soradore2@gmail.com>
2025-12-01 19:54:51 +0900

    Update vite.config.app.js

--- vite.config.app.js
+++ vite.config.app.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import keystonePlugin from 'keystone/vite-plugin';
+import keystonePlugin from '@gollilla/keystone/vite-plugin';
 
 export default defineConfig({
   plugins: [keystonePlugin({ uuid: "82b9174d-ea3d-42ae-986c-b42ce3644760" })],
